76c410e5943b6048243a17219cc63f3bf35a5527
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2021-05-23 21:21:11 +0800

    update

---
+++ script/Config.lua
@@ -0,0 +1,11 @@
+mWindow = UsingModule("Window")
+
+return {
+    RC_WINDOW = {
+        x = mWindow.WINDOW_POSITION_DEFAULT,
+        y = mWindow.WINDOW_POSITION_DEFAULT,
+        w = 1280,
+        h = 720
+    },
+    N_FPS = 144
+}
